ef00a786119d26860d14bac9a3a7c711a4389b0d
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-02-06 08:11:46 +1100

    enumerate object actions;

---
+++ Sources/Amatino/Action.swift
@@ -0,0 +1,16 @@
+//
+//  Amatino Swift
+//  Action.swift
+//
+//  author: hugh@blinkybeach.com
+//
+
+import Foundation
+
+internal enum Action {
+    case Retrieve
+    case Create
+    case Update
+    case Delete
+    case Restore
+}
